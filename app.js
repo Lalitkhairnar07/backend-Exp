b9c0cb8b2cc1cf22baeb72a64f5fbf58c227f3c8
khairnaralalit07 <khairnarlalit845@gmail.com>
2026-04-14 21:04:32 +0530

    server

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
+const cors = require("cors")
+app.use(cors())
+
 
 const userRoutes = require("./src/routes/UserRoutes")
 app.use("/user",userRoutes)

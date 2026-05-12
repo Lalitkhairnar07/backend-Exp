cf2b48d3bc21df4795b4ffb69eb14cf42c1d6e63
khairnaralalit07 <khairnarlalit845@gmail.com>
2026-05-13 00:04:05 +0530

    expCatmodel

--- src/models/ExpenseCategoryModel.js
+++ src/models/ExpenseCategoryModel.js
@@ -2,17 +2,29 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 const expCategorySchema = new Schema({
 
-    catName:{
-        type:String
+    catName: {
+        type: String
     },
-    description:{
-        type:String
+    description: {
+        type: String
     },
-    userId:{
-        type:Schema.Types.ObjectId,
-        ref:"users"
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
     }
 
 })
 
-module.exports = mongoose.model("expCategory",expCategorySchema)
+expCategorySchema.pre("findOneAndDelete", async function () {
+
+    const category = await this.model.findOne(this.getFilter())
+
+    if (category) {
+        await mongoose.model("expenses").deleteMany({
+            expCategory: category._id
+        })
+        console.log("deleted related expenses from category")
+    }
+
+})
+module.exports = mongoose.model("expCategory", expCategorySchema)

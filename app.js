e9022dae0544c723ed2ab3dde39ca6e21169595a
khairnaralalit07 <khairnarlalit845@gmail.com>
2026-05-13 00:02:22 +0530

    server

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.use("/user",userRoutes)
 const expCategoryRoutes = require("./src/routes/ExpCategoryRoutes")
 app.use("/expenseCategory",expCategoryRoutes)
 
+// const expenseRoutes = require("./src/routes/ExpenseRoutes")
+// app.use("/exp",expenseRoutes)
+
+const expenseRoutes = require("./src/routes/ExpenseRoutes")
+app.use("/exp",expenseRoutes)
+
 
 //DBCONNECTION:
 const DBConnection = require("./src/utils/DBConnection")

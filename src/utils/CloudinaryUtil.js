c901c92040b85a2ae8090eed5b92859101aaae59
khairnaralalit07 <khairnarlalit845@gmail.com>
2026-05-15 13:13:18 +0530

    utils

---
+++ src/utils/CloudinaryUtil.js
@@ -0,0 +1,20 @@
+const cloudinary = require("cloudinary").v2
+require("dotenv").config()
+
+
+const uploadFileToCloudinary = async(filePath)=>{
+
+    cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+})
+
+const cloudinaryResponse = await cloudinary.uploader.upload(filePath)
+return cloudinaryResponse;
+
+
+}
+
+
+module.exports = uploadFileToCloudinary

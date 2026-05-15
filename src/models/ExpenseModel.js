e1a5a15c3ac81d81c98e657d66987fd60d7da421
khairnaralalit07 <khairnarlalit845@gmail.com>
2026-05-15 13:12:51 +0530

    model

--- src/models/ExpenseModel.js
+++ src/models/ExpenseModel.js
@@ -30,7 +30,13 @@ const expenseSchema = new Schema({
         type:String,
         enum:["UPI","CASH","CARD","CHECK","EMI"]
     },
-
+    income:{
+        type:Number
+    },
+    incomeCategory:{
+        type:Schema.Types.ObjectId,
+        ref:"incomeCategory"
+    }
     
 },{timestamps:true})
 
